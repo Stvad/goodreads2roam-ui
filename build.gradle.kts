57049f9e7fade29c41d80785cb6a4ca586a37778
Vladyslav Sitalo <sitalov@amazon.com>
2021-03-27 20:49:01 -0700

    bump clojure lib version

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     //Share Buttons (chapter 7)
     implementation(npm("react-share", "~4.2.1"))
     // implementation(npm(File("/Users/sitalov/Dropbox/SoftwareEngineering/Projects/Roam/goodreads2roam")))
-    implementation(npm("goodreads2roam", "~0.1.0"))
+    implementation(npm("goodreads2roam", "~0.1.1"))
 
     //Coroutines (chapter 8)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
